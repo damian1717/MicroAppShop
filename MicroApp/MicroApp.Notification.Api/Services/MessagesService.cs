24a673fd5c3705463697789c0a5f3e58b0f6268e
damian1717 <damiancichy17@gmail.com>
2020-04-22 18:32:46 +0200

    added logging for sending mail

--- MicroApp/MicroApp.Notification.Api/Services/MessagesService.cs
+++ MicroApp/MicroApp.Notification.Api/Services/MessagesService.cs
@@ -1,5 +1,6 @@
 ﻿using MailKit.Net.Smtp;
 using MicroApp.Common.MailKit;
+using Microsoft.Extensions.Logging;
 using MimeKit;
 using System.Threading.Tasks;
 
@@ -8,14 +9,18 @@ namespace MicroApp.Notification.Api.Services
     public class MessagesService : IMessagesService
     {
         private readonly MailKitOptions _options;
+        private readonly ILogger<MessagesService> _logger;
 
-        public MessagesService(MailKitOptions options)
+        public MessagesService(MailKitOptions options, ILogger<MessagesService> logger)
         {
             _options = options;
+            _logger = logger;
         }
 
         public async Task SendAsync(MimeMessage message)
         {
+            _logger.LogInformation($"Sending an email message to: '{message.To}'.");
+
             using (var client = new SmtpClient())
             {
                 client.Connect(_options.SmtpHost, _options.Port, true);
@@ -24,6 +29,8 @@ namespace MicroApp.Notification.Api.Services
                 await client.SendAsync(message);
                 client.Disconnect(true);
             }
+
+            _logger.LogInformation($"Sent an email message to: '{message.To}'.");
         }
     }
 }
